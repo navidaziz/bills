75634651cfacb011e62d6b9f32a7e0e5cd087d01
navidaziz98 <navidaziz98@gmail.com>
2024-10-03 20:20:10 +0500

    some changes

--- application/views/admin/billing_months/print_billing_month.php
+++ application/views/admin/billing_months/print_billing_month.php
@@ -17,6 +17,7 @@
 ?>
 <head>
     <meta charset="utf-8">
+    <meta name="robots" content="noindex, nofollow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <meta name="keywords" content="electricity, bill, consumer, utilities">
